6d6174ae3044688f9e74972a805bc7185d8dcc9a
Mritunjay Upadhyay <mupadhyay00@gmail.com>
2022-10-03 19:58:37 +0700

    making merge conflict

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <p>First step: Push code to git hub</p>
-      <p>Second step: cloned code from github, now editing the code and pushing back</p>
+      <p>Third Step: Editing for merge conflict.</p>
     </div>
   );
 }
